4b40b71fd79d36459fa380916dc09c44ca3c087e
uelei <github@uelei.com>
2022-03-06 21:19:52 -0300

    :alembic: test some configs

--- neovim/config.lua
+++ neovim/config.lua
@@ -19,9 +19,9 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- coment like vscode
-lvim.keys.visual_mode['<C-_>'] = '<CMD>lua require("Comment.api").toggle_current_linewise()<CR>'
-lvim.keys.normal_mode['<C-_>'] = '<CMD>lua require("Comment.api").toggle_current_linewise()<CR>'
-lvim.keys.insert_mode['<C-_>'] = '<CMD>lua require("Comment.api").toggle_current_linewise()<CR>'
+lvim.keys.visual_mode["<C-_>"] = "<ESC><CMD>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>"
+lvim.keys.normal_mode["<C-_>"] = "<ESC><CMD>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>"
+lvim.keys.insert_mode["<C-_>"] = "<ESC><CMD>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>"
 
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = false
@@ -120,32 +120,30 @@ lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Project
 -- 	{ "J", ":move '>+1<CR>gv-gv" },
 -- }
 
-
-
 -- *
 -- Telescope
 -- *
 lvim.builtin.telescope.active = true
 lvim.builtin.telescope.defaults.file_ignore_patterns = { ".git", "node_modules" }
 local get_telescope_mappings = function()
-    local actions = require("telescope.actions")
-    return {
-  i = {
-   ["<C-n>"] = actions.cycle_history_next,
-   ["<C-p>"] = actions.cycle_history_prev,
-   ["<C-c>"] = actions.close,
-   ["<C-j>"] = actions.move_selection_next,
-   ["<C-k>"] = actions.move_selection_previous,
-   ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-   ["<CR>"] = actions.select_default + actions.center,
-   ["<c-x>"] = false,
-  },
-  n = {
-   ["<C-j>"] = actions.move_selection_next,
-   ["<C-k>"] = actions.move_selection_previous,
-   ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-  },
-    }
+	local actions = require("telescope.actions")
+	return {
+		i = {
+			["<C-n>"] = actions.cycle_history_next,
+			["<C-p>"] = actions.cycle_history_prev,
+			["<C-c>"] = actions.close,
+			["<C-j>"] = actions.move_selection_next,
+			["<C-k>"] = actions.move_selection_previous,
+			["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+			["<CR>"] = actions.select_default + actions.center,
+			["<c-x>"] = false,
+		},
+		n = {
+			["<C-j>"] = actions.move_selection_next,
+			["<C-k>"] = actions.move_selection_previous,
+			["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+		},
+	}
 end
 lvim.builtin.telescope.defaults.mappings = get_telescope_mappings()
 
@@ -153,7 +151,7 @@ lvim.builtin.telescope.defaults.mappings = get_telescope_mappings()
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.notify.active = true
 
-
+-- *
 -- Dashboard
 -- *
 lvim.builtin.dashboard.active = true
@@ -182,15 +180,13 @@ lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.matchup.enable = true
 lvim.builtin.treesitter.context_commentstring.enable = true
 
-
 -- *
 -- Whichkey
 -- *
 lvim.builtin.which_key.active = true
 lvim.builtin.which_key.mappings["w"] = { "<cmd>w<CR>", "Save" }
--- lvim.builtin.which_key.mappings["W"] = { "<cmd>w!<CR>", "Force Save" }
 lvim.builtin.which_key.mappings["q"] = { "<cmd>q<CR>", "Quit" }
--- lvim.builtin.which_key.mappings["Q"] = { "<cmd>q!<CR>", "Force Quit" }
+
 -- lvim.builtin.which_key.mappings["F"] = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" }
 -- lvim.builtin.which_key.mappings["b"]["c"] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Search Current Buffer" }
 -- lvim.builtin.which_key.mappings["s"]["f"] = {
@@ -199,45 +195,45 @@ lvim.builtin.which_key.mappings["q"] = { "<cmd>q<CR>", "Quit" }
 -- }
 -- lvim.builtin.which_key.mappings["s"]["m"] = { "<cmd>Telescope marks<cr>", "Search Marks" }
 -- lvim.builtin.which_key.mappings["s"]["g"] = { "<cmd>Telescope git_files<cr>", "Search Git Files" }
-lvim.builtin.which_key.mappings["g"]["g"] = {"<cmd>lua require('neogit').open()<cr>", "NeoGit"}
-lvim.builtin.which_key.mappings["b"]["c"] = {"<cmd>BufferKill<CR>", "Close Buffer"}
+lvim.builtin.which_key.mappings["g"]["g"] = { "<cmd>lua require('neogit').open()<cr>", "NeoGit" }
+lvim.builtin.which_key.mappings["b"]["c"] = { "<cmd>BufferKill<CR>", "Close Buffer" }
 
-lvim.builtin.which_key.mappings["t"] = {
- name = "Toggle",
- h = { "<cmd>set hlsearch!<CR>", "Toggle Highlight" },
- q = { "<cmd>call QuickFixToggle()<CR>", "Toggle Quick Fix List" },
- b = { "<cmd>GitBlameToggle<CR>", "Toggle Git Blame" },
- t = { "<cmd>Twilight<CR>", "Toggle Twilight" },
- i = { "<cmd>IndentBlanklineToggle<CR>", "Toggle Indent Line" },
- x = { "<cmd>TroubleToggle<CR>", "Toggle Trouble" },
-}
+-- lvim.builtin.which_key.mappings["t"] = {
+-- 	name = "Toggle",
+-- 	h = { "<cmd>set hlsearch!<CR>", "Toggle Highlight" },
+-- 	q = { "<cmd>call QuickFixToggle()<CR>", "Toggle Quick Fix List" },
+-- 	b = { "<cmd>GitBlameToggle<CR>", "Toggle Git Blame" },
+-- 	t = { "<cmd>Twilight<CR>", "Toggle Twilight" },
+-- 	i = { "<cmd>IndentBlanklineToggle<CR>", "Toggle Indent Line" },
+-- 	x = { "<cmd>TroubleToggle<CR>", "Toggle Trouble" },
+-- }
 -- lvim.builtin.which_key.mappings["z"] = { "<cmd>ZenMode<CR>", "Zen Mode" }
 lvim.builtin.which_key.mappings["x"] = {
- name = "Trouble",
- w = { "<cmd>Trouble lsp_workspace_diagnostics<CR>", "Trouble Workspaces" },
- d = { "<cmd>Trouble lsp_document_diagnostics<CR>", "Trouble Document" },
- l = { "<cmd>Trouble loclist<CR>", "Trouble Location List" },
- q = { "<cmd>Trouble quickfix<CR>", "Trouble Quickfix List" },
+	name = "Trouble",
+	w = { "<cmd>Trouble workspace_diagnostics<CR>", "Trouble Workspaces" },
+	d = { "<cmd>Trouble document_diagnostics<CR>", "Trouble Document" },
+	l = { "<cmd>Trouble loclist<CR>", "Trouble Location List" },
+	q = { "<cmd>Trouble quickfix<CR>", "Trouble Quickfix List" },
 }
 -- Dap
 lvim.builtin.dap.active = true
-require('dap.ext.vscode').load_launchjs()
+require("dap.ext.vscode").load_launchjs()
 
 -- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
-}
+-- lvim.builtin.treesitter.ensure_installed = {
+-- 	"bash",
+-- 	"c",
+-- 	"javascript",
+-- 	"json",
+-- 	"lua",
+-- 	"python",
+-- 	"typescript",
+-- 	"tsx",
+-- 	"css",
+-- 	"rust",
+-- 	"java",
+-- 	"yaml",
+-- }
 
 -- generic LSP settings
 
@@ -263,123 +259,123 @@ lvim.builtin.treesitter.ensure_installed = {
 -- end
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  -- { command = "black", filetypes = { "python" } },
-  { command = "isort", filetypes = { "python" } },
-  {
-    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--print-with", "100" },
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact" },
-  },
-}
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "isort", filetypes = { "python" } },
+	{ command = "black", filetypes = { "python" } },
+	{
+		-- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+		command = "prettier",
+		---@usage arguments to pass to the formatter
+		-- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+		extra_args = { "--print-with", "100" },
+		---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+		filetypes = { "typescript", "typescriptreact" },
+	},
+})
 
 -- -- set additional linters
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { command = "flake8", filetypes = { "python" } },
-  {
-    -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "shellcheck",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--severity", "warning" },
-  },
-  -- {
-  --   command = "codespell",
-  --   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --   filetypes = { "javascript", "python" },
-  -- },
-}
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{ command = "flake8", filetypes = { "python" } },
+	{
+		-- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+		command = "shellcheck",
+		---@usage arguments to pass to the formatter
+		-- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+		extra_args = { "--severity", "warning" },
+	},
+	-- {
+	--   command = "codespell",
+	--   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+	--   filetypes = { "javascript", "python" },
+	-- },
+})
 
 -- Additional Plugins
 lvim.plugins = {
-   {
-  "f-person/git-blame.nvim",
-  event = "BufRead",
-  config = function()
-      vim.cmd("highlight default link gitblame SpecialComment")
-      vim.g.gitblame_enabled = 0
-  end,
-   },
-   -- { "kdheepak/lazygit.nvim" },
-{'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' },
-   {
-  "NTBBloodbath/rest.nvim",
-  requires = { "nvim-lua/plenary.nvim" },
-  config = function()
-      require("rest-nvim").setup({
-      -- Open request results in a horizontal split
-      result_split_horizontal = false,
-      -- Skip SSL verification, useful for unknown certificates
-      skip_ssl_verification = false,
-      -- Highlight request on run
-      highlight = {
-        enabled = true,
-        timeout = 150,
-      },
-      result = {
-        -- toggle showing URL, HTTP info, headers at top the of result window
-        show_url = true,
-        show_http_info = true,
-        show_headers = true,
-      },
-      -- Jump to request line on run
-      jump_to_request = false,
-      env_file = '.env',
-      custom_dynamic_variables = {},
-      yank_dry_run = true,
-      })
-  end
-   },
-   {
-  "folke/trouble.nvim",
-  requires = "kyazdani42/nvim-web-devicons",
-  config = function()
-      require("trouble").setup({
-    position = "bottom", -- position of the list can be: bottom, top, left, right
-    height = 10, -- height of the trouble list when position is top or bottom
-    width = 50, -- width of the list when position is left or right
-    icons = true, -- use devicons for filenames
-    mode = "document_diagnostics",
-    action_keys = { -- key mappings for actions in the trouble list
-                    -- map to {} to remove a mapping, for example:
-                    -- close = {},
-                    close = "q", -- close the list
-                    cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-                    refresh = "r", -- manually refresh
-                    jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
-                    open_split = { "<c-x>" }, -- open buffer in new split
-                    open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
-                    open_tab = { "<c-t>" }, -- open buffer in new tab
-                    jump_close = { "o" }, -- jump to the diagnostic and close the list
-                    toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
-                    toggle_preview = "P", -- toggle auto_preview
-                    hover = "K", -- opens a small popup with the full multiline message
-                    preview = "p", -- preview the diagnostic location
-                    close_folds = { "zM", "zm" }, -- close all folds
-                    open_folds = { "zR", "zr" }, -- open all folds
-                    toggle_fold = { "zA", "za" }, -- toggle fold of current file
-                    previous = "k", -- preview item
-                    next = "j", -- next item
-    },
-    indent_lines = true, -- add an indent guide below the fold icons
-    auto_open = false, -- automatically open the list when you have diagnostics
-    auto_close = true, -- automatically close the list when you have no diagnostics
-    auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-    auto_fold = false, -- automatically fold a file trouble list at creation
-    use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
-      })
-  end,
-   },
-
+	{
+		"f-person/git-blame.nvim",
+		event = "BufRead",
+		config = function()
+			vim.cmd("highlight default link gitblame SpecialComment")
+			vim.g.gitblame_enabled = 0
+		end,
+	},
+	{ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" },
+	{
+		"NTBBloodbath/rest.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("rest-nvim").setup({
+				-- Open request results in a horizontal split
+				result_split_horizontal = false,
+				-- Skip SSL verification, useful for unknown certificates
+				skip_ssl_verification = false,
+				-- Highlight request on run
+				highlight = {
+					enabled = true,
+					timeout = 150,
+				},
+				result = {
+					-- toggle showing URL, HTTP info, headers at top the of result window
+					show_url = true,
+					show_http_info = true,
+					show_headers = true,
+				},
+				-- Jump to request line on run
+				jump_to_request = false,
+				env_file = ".env",
+				custom_dynamic_variables = {},
+				yank_dry_run = true,
+			})
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				position = "bottom", -- position of the list can be: bottom, top, left, right
+				height = 10, -- height of the trouble list when position is top or bottom
+				width = 50, -- width of the list when position is left or right
+				icons = true, -- use devicons for filenames
+				mode = "document_diagnostics",
+				action_keys = { -- key mappings for actions in the trouble list
+					-- map to {} to remove a mapping, for example:
+					-- close = {},
+					close = "q", -- close the list
+					cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+					refresh = "r", -- manually refresh
+					jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
+					open_split = { "<c-x>" }, -- open buffer in new split
+					open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+					open_tab = { "<c-t>" }, -- open buffer in new tab
+					jump_close = { "o" }, -- jump to the diagnostic and close the list
+					toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+					toggle_preview = "P", -- toggle auto_preview
+					hover = "K", -- opens a small popup with the full multiline message
+					preview = "p", -- preview the diagnostic location
+					close_folds = { "zM", "zm" }, -- close all folds
+					open_folds = { "zR", "zr" }, -- open all folds
+					toggle_fold = { "zA", "za" }, -- toggle fold of current file
+					previous = "k", -- preview item
+					next = "j", -- next item
+				},
+				indent_lines = true, -- add an indent guide below the fold icons
+				auto_open = false, -- automatically open the list when you have diagnostics
+				auto_close = true, -- automatically close the list when you have no diagnostics
+				auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+				auto_fold = false, -- automatically fold a file trouble list at creation
+				use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
+			})
+		end,
+	},
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
+
+require("settings.lua")
