cc18981e46591820b96734100cbddb177212fbef
Wesley Werneck <wesley.werneck@ebury.com>
2024-01-09 17:45:40 -0300

    try new font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,7 +11,7 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font 'MesloLGS Nerd Font'
+config.font = wezterm.font 'CaskaydiaCove Nerd Font'
 
 config.hide_tab_bar_if_only_one_tab = true
 -- For example, changing the color scheme:
