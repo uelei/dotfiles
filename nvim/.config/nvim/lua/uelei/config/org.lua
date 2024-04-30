1ce8a53319f71eca82c59befa256911e09b2ec92
uelei <uelei@ueleis-MacBook-Pro.local>
2024-04-30 17:39:40 -0300

    remove warning on org.nvim

--- nvim/.config/nvim/lua/uelei/config/org.lua
+++ nvim/.config/nvim/lua/uelei/config/org.lua
@@ -7,19 +7,6 @@ end
 
 local Menu = require 'org-modern.menu'
 
--- Load custom tree-sitter grammar for org filetype
-orgmode.setup_ts_grammar()
-
--- Tree-sitter configuration
--- require("nvim-treesitter.configs").setup({
---     -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
---     highlight = {
---         enable = true,
---         additional_vim_regex_highlighting = { "org" }, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
---     },
---     ensure_installed = { "org" },                -- Or run :TSUpdate org
--- })
-
 orgmode.setup {
 
   org_agenda_skip_scheduled_if_done = true,
