05910377ade741b722c7c24c1b2063ab22ae9e0e
Wesley Werneck <github@uelei.com>
2024-12-13 17:34:25 -0300

    🧑‍💻 Add mason-lsp-config to help install lsp and lint

--- nvim/.config/nvim/lua/uelei/plugins/mason.lua
+++ nvim/.config/nvim/lua/uelei/plugins/mason.lua
@@ -2,6 +2,7 @@ return {
     {
         'williamboman/mason.nvim',
         dependencies = {
+            'williamboman/mason-lspconfig.nvim',
             'WhoIsSethDaniel/mason-tool-installer.nvim',
         },
         config = function()
