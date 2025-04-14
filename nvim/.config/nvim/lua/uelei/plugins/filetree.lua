3c160c68a8ffa2efa3d758874ff3fb9334b7a525
Wesley Werneck <wesley.werneck@ebury.com>
2025-04-14 13:05:33 -0300

    some changes

--- nvim/.config/nvim/lua/uelei/plugins/filetree.lua
+++ nvim/.config/nvim/lua/uelei/plugins/filetree.lua
@@ -7,9 +7,14 @@ return {
             'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
             'MunifTanjim/nui.nvim',
         },
-        config = function()
-            require('neo-tree').setup {}
-        end,
+        opts = {
+
+            follow_current_file = { enabled = true },
+        },
+        -- config = function()
+        --     require('neo-tree').setup {
+        --     }
+        -- end,
         keys = {
             { '<leader>fe', '<cmd>Neotree toggle<cr>', desc = 'Open File NeoTree' },
         },
